0d51be411569de02a81452e12fa79f5359bc32d2
matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
2016-05-10 17:59:59 +0900

    Add files via upload

---
+++ BootApp/src/main/java/com/tuyano/libro/bootapp/SampleController.java
@@ -0,0 +1,16 @@
+package com.tuyano.libro.bootapp;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class SampleController {
+ 
+    @RequestMapping("/helo")
+    public String helo(Model model) {
+        model.addAttribute("val", "text in Controller");
+    return "helo";
+    }
+ 
+}
